0397f5e18763e955a601d49b19356e9b96d00b86
danieltrusca <truscadani@yahoo.com>
2023-01-26 18:52:39 +0200

    the starter project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// import pages
+import Home from "./pages/Home"
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <div style={{height:'2000px'}}>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
